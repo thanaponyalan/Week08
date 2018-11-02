108790d38f17746e94066d2c3b65fd219838b570
thanaponyalan <thanaponyalan@gmail.com>
2018-11-02 22:31:59 +0700

    2.1.4

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,21 +10,13 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            Student[] student = new Student[31];
-            Console.WriteLine(data.Length);
-            Console.WriteLine(student.Length);
+            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
+            Console.WriteLine("Length of data = {0}", data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
             Console.ReadLine();
         }
-        class Student     // nested class
-        {
-            private string name;
-            public string Name
-            {
-                get { return name; }
-                set { name = value; }
-            }
-        }
-
     }
 }
