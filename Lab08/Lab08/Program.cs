b1403635d4ac3eeeb6496eae49e5c7c5c76ab5b7
thanaponyalan <thanaponyalan@gmail.com>
2018-11-05 05:34:00 +0700

    GPA/GPS

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -18,19 +18,18 @@ namespace Lab08
             {
                 Console.Write("Enter semester {0}'s GPS : ",i+1);
                 GPS[i] = double.Parse(Console.ReadLine());
-                Console.Write("Enter semester {0}'s Weight",i+1);
+                Console.Write("Enter semester {0}'s Weight : ",i+1);
                 Weight[i] = double.Parse(Console.ReadLine());
                 tmpGPS[i] = GPS[i] * Weight[i];
             }
-
             double GPA = tmpGPS.Sum() / Weight.Sum();
             int j = 0;
             foreach (double g in GPS)
             {
                 j++;
-                Console.WriteLine("Semester {0} GPS is {1}",j,g);
+                Console.WriteLine("Semester {0} GPS is {1:F2}",j,g);
             }
-            Console.WriteLine("GPA : {0} ",GPA);
+            Console.WriteLine("GPA : {0:F2} ",GPA);
             Console.ReadKey();
 
         }
