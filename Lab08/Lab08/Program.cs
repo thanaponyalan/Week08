758aafb5b8836fb83ab088c5df6c4cc2d2e03c62
thanaponyalan <thanaponyalan@gmail.com>
2018-11-05 07:18:24 +0700

    2.2.2 Practice 1

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -10,47 +10,22 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-            // Array of characters.
-            char[] array1 = { 'q', 'w', 'e', 'r', 't',
-                'y', 'u', 'i', 'o', 'p' };
-            Array.Sort(array1);
-
-            // Array of strings.
-            string[] colors = new string[] { "red",
-              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
-            Array.Sort(colors);
-
-            string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
-              "Indonesia", "Singapore", "Thailand", "Philippines",
-              "Vietnam", "Brunei Darussalam", "Laos", "Myanmar" };
-
-            // C# program that uses LINQ
-            var sortAscending = from country in AseanCountries
-                                orderby country
-                                select country;
-            var sortDescending = from country in AseanCountries
-                                 orderby country descending
-                                 select country;
-
-            // print output
-            Console.WriteLine("------Character sorting----------");
-            foreach (var c in array1)
+            string[] Months = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            var sortAscending = from month in Months
+                                orderby month
+                                select month;
+            var sortDescending = from month in Months orderby month descending select month;
+            Console.WriteLine("*****Ascending*****");
+            foreach (var c in sortAscending)
+            {
                 Console.WriteLine(c);
-
-            Console.WriteLine("------String sorting----------");
-            foreach (string color in colors)
-                Console.WriteLine(color);
-
-            Console.WriteLine("------String sort ascending----------");
-            foreach (string c in sortAscending)
+            }
+            Console.WriteLine("\n*****Descending*****");
+            foreach (var c in sortDescending)
+            {
                 Console.WriteLine(c);
-
-            Console.WriteLine("------String sort descending----------");
-            foreach (string c in sortDescending)
-                Console.WriteLine(c);
-            // wait
-            Console.ReadLine();
-
+            }
+            Console.ReadKey();
         }
     }
 }
